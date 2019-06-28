2c038e15a67c8ec57fac80eec35d33ad115f0fe7
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-06-28 08:33:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+alan_known_for = programmer_hash[:alan_kay][:known_for]
+p alan_known_for
 end
 
 def dennis_ritchies_language
